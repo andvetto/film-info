f694b15c2220218bfcad06cbc280aa18779fe99a
andvetto86 <andvetto86@gmail.com>
2019-11-20 16:43:40 +0100

    search button padding

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -10,7 +10,7 @@ function NavBar(props){
             <nav className="navbar navbar-light bg-light justify-content-center">
 
                 <input className="form-control col-md-4" onChange={props.onChange} type="text" placeholder="Search" aria-label="Search"/>
-                <button className="btn btn-outline-success" onClick={props.onClick} >Search</button>
+                <button className="btn btn-outline-success mx-2" onClick={props.onClick} >Search</button>
             </nav>
         )
   
