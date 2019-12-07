d8e70d8ae9f38f0a73c73fdb8fa3d2c2f3969042
andvetto <andvetto86@gmail.com>
2019-12-07 12:39:18 +0100

    Updates

---
+++ precache-manifest.f3e8df0f67aca885f14b92f10f629b04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3f452698192dc89e07ec3aea6543bdf",
+    "url": "/film-info/index.html"
+  },
+  {
+    "revision": "4f09fde833c2daf835ea",
+    "url": "/film-info/static/css/main.57027637.chunk.css"
+  },
+  {
+    "revision": "b1bd7a40d07d3ab029de",
+    "url": "/film-info/static/js/2.98621a98.chunk.js"
+  },
+  {
+    "revision": "4f09fde833c2daf835ea",
+    "url": "/film-info/static/js/main.86b74f20.chunk.js"
+  },
+  {
+    "revision": "6da6fc524f07f89a4f21",
+    "url": "/film-info/static/js/runtime-main.f8f7b400.js"
+  }
+]);
